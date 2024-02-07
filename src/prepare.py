67ce1a5ef755395d9a26518b861784c6991ddfab
Thomas Fraunholz <tf@sourcemaker.org>
2024-02-07 16:51:23 +0000

    solved dvc s3

--- src/prepare.py
+++ src/prepare.py
@@ -1,44 +1,5 @@
 import dvc.api
-from datasets import DatasetDict, Dataset, load_from_disk
-from pathlib import Path
-from shared.helpers import write_json
-from collections import Counter
-from shared.data import get_labels
-
-def train_test_split(dataset, **kwargs):
-    dev_test_split = dataset.train_test_split(
-        test_size=kwargs["test-size"], 
-        seed=kwargs["seed"]
-    )
-    train_eval_split = dev_test_split["train"].train_test_split(
-        train_size=kwargs["train-size"], 
-        seed=kwargs["seed"]
-    )
-    dataset = DatasetDict(
-        {
-            "train": train_eval_split["train"],
-            "validate": train_eval_split["test"],
-            "test": dev_test_split["test"],
-        }
-    )
-    return dataset
-
-
-def get_metrics(dataset: Dataset, **kwargs):
-    metrics = {}
-    labels = get_labels(dataset["test"])
-    for split_name, split in dataset.items():
-        samples_per_label = dict(Counter(split['label']))
-        # replace key of samples_per_label with key of labels using the value if labels
-        labels_per_label = {labels[str(key)]: value for key, value in samples_per_label.items()}
-        # alphabetically sort labels_per_label
-        labels_per_label = dict(sorted(labels_per_label.items()))
-        metrics[split_name] = {
-            'size': len(split),
-            'labels': labels_per_label,
-        }
-    return metrics
-
+import 
 
 def prepare(dataset: Dataset, **kwargs):
     splitted_dataset = train_test_split(
